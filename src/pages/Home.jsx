b20cad78fc51253558be83c56e18035a1ec55dea
Antor-Chowdhury <antor1347@gmail.com>
2025-10-24 17:48:59 +0600

    added banner and state section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Banner from "../components/Banner/Banner";
+import State from "../components/State/State";
 
 const Home = () => {
   return (
     <div>
-      <h1>I am Home</h1>
+      <div className="max-w-[1440px] mx-auto">
+        <Banner />
+      </div>
+      <State></State>
     </div>
   );
 };
